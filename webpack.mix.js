71bf595a7072d7923e66c75e63eba5a17cd588df
Jeremy Cook <jeremycook0@icloud.com>
2019-10-31 10:31:37 -0400

    Fix linting issues causing build problem.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,12 @@
 const mix = require('laravel-mix');
 const vueConfig = require('./vue.config.js');
+const path = require('path');
 let webpackConfig = vueConfig.configureWebpack;
 
 webpackConfig.resolve.alias['@'] = path.resolve(__dirname, 'src');
-webpackConfig.externals = { 'vue' : 'Vue' };
+webpackConfig.externals = { 'vue': 'Vue' };
 
-console.log(webpackConfig);
 mix.webpackConfig(webpackConfig).setPublicPath('public')
-    .js('resources/js/index.js', 'js')
-    .js('resources/js/initialLoad.js', 'js')
-    .copy("dist/img", "public/js/img");
+  .js('resources/js/index.js', 'js')
+  .js('resources/js/initialLoad.js', 'js')
+  .copy('dist/img', 'public/js/img');
