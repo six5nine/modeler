28186a263a12554fb1dd04bae687ee230bdc1981
Jeremy Cook <jeremycook0@icloud.com>
2019-11-01 13:44:01 -0400

    Add files that were missed to the modeler package.

--- src-php/Http/Controllers/ModelerController.php
+++ src-php/Http/Controllers/ModelerController.php
@@ -5,21 +5,22 @@ namespace ProcessMaker\Package\Modeler\Http\Controllers;
 use Illuminate\Http\Request;
 use ProcessMaker\Http\Controllers\Controller;
 use ProcessMaker\Models\Process;
-
-// TODO move to this package:
-use ProcessMaker\Managers\ModelerManager;
-use ProcessMaker\Events\ModelerStarting;
+use ProcessMaker\Package\Modeler\Events\ModelerStarting;
+use ProcessMaker\Package\Modeler\Managers\ModelerManager;
 
 class ModelerController extends Controller
 {
 
     /**
      * Invokes the Process Modeler for rendering.
+     * @param ModelerManager $manager
+     * @param Process $process
+     * @return
      */
     public function __invoke(ModelerManager $manager, Process $process)
     {
         /**
-         * Emit the ModelerStarting event, passing in our ModelerManager instance. This will 
+         * Emit the ModelerStarting event, passing in our ModelerManager instance. This will
          * allow packages to add additional javascript for modeler initialization which
          * can customize the modeler controls list.
          */
