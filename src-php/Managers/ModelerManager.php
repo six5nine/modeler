2c3492d1b3e871d5993f56cc5b31c371e32fb925
Nolan Ehrstrom <github@nolanpro.com>
2019-11-04 11:26:08 -0800

    Fix ModelerManager and add missing manifest

--- src-php/Managers/ModelerManager.php
+++ src-php/Managers/ModelerManager.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace ProcessMaker\Package\Modeler\Managers;
 
 class ModelerManager
@@ -7,20 +6,20 @@ class ModelerManager
     private $javascriptRegistry;
 
     /**
-     * Start our modeler manager, registering our initial javascript
+     * Start our modeler manager, registering our initial javascript 
      * which will add our intial node types to the modeler (base bpmn types/shapes)
      */
     public function __construct()
     {
         $this->javascriptRegistry = [];
         // Include our default javascript for our core controls
-        $this->addScript(mix('js/processes/modeler/initialLoad.js'));
+        $this->addScript(mix('js/initialLoad.js', 'vendor/processmaker/packages/package-modeler'));
     }
 
     /**
-     * Add a new script to the modeler load.  These scripts can then interact with the modeler
+     * Add a new script to the modeler load.  These scripts can then interact with the modeler 
      * during it's startup lifecycle to do this such as register new node types.
-     *
+     * 
      * @param string $script Path to the javascript to load
      * @return void
      */
@@ -30,9 +29,9 @@ class ModelerManager
     }
 
     /**
-     * Retrieve the list of scripts that have been added. This is used in the modeler blade
+     * Retrieve the list of scripts that have been added. This is used in the modeler blade 
      * to execute each script in a script tag before the modeler is started.
-     *
+     * 
      * @return array Collection of paths to scripts to load
      */
     public function getScripts()
